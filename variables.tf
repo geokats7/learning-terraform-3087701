d966afda4b3de63857c11ffb601776835b628c6f
Yorgos Katsaros <katsaros@orfium.com>
2022-12-29 22:56:14 +0200

    aws variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,11 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  default = ""
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  default = ""
+}
